729fc6eea141112f844aa10456e5f573ab2b50cb
thameem-cavininfotech <thameem.s@cavininfotech.com>
2024-10-13 23:27:43 +0530

    fix: cors issue

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ import route from "./routes/classRoutes.js";
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 const MONGOURL = process.env.MONGO_URL;
